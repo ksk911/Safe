ac449d4ec17ca1a7c0125813b1626890c157b1d4
ksk911 <kaustubhskhisti@gmail.com>
2025-01-19 00:04:07 +0530

    commit

---
+++ android/app/src/main/kotlin/com/example/learningdart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.safe.app;
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:ui';
+import "package:firebase_auth/firebase_auth.dart";
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Register',
+              //textAlign: TextAlign.center,
+            )),
+        backgroundColor: const Color.fromARGB(255, 251, 64, 145),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            /* enableSuggestions: false, */
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(hintText: 'TYPE YOUR EMAIL'),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(hintText: 'Enter your password'),
+          ),
+          TextButton(
+              onPressed: () async {
+                await Firebase.initializeApp(
+                    options: DefaultFirebaseOptions.currentPlatform);
+                //print('helo world');
+
+                final email = _email.text;
+                final password = _password.text;
+                print(password);
+                final usercredential = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password);
+                print(usercredential);
+              },
+              child: const Text('Resgister Now!!')),
+        ],
+      ),
+    );
+  }
+}
